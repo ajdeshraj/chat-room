f3e55060958789d5383ffa609113118fc40ab045
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-08-05 16:15:22 +0530

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+
+const publicDirPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirPath))
+
+app.listen(3000, () => {
+    console.log('Server is Running on Port 3000')
+})
