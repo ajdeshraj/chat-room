676f5d9b4138c15bb14d609c4e29e3155d36efea
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-08-06 12:27:17 +0530

    Added basic support for web sockets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const publicDirPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirPath))
 
-app.listen(3000, () => {
+io.on('connection', () => {
+    console.log('New Web Socket Connection')
+})
+
+server.listen(3000, () => {
     console.log('Server is Running on Port 3000')
 })
